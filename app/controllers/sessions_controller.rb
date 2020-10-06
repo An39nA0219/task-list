6ca6f457a077e719cc6ad68454047fff0d92f5f6
EC2 Default User <ec2-user@ip-172-31-43-41.ec2.internal>
2020-10-06 00:22:11 +0000

    user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,36 @@ class SessionsController < ApplicationController
   end
 
   def create
+    email = params[:session][:email].downcase
+    password = params[:session][:password]
+    
+    if login(email, password)
+      flash[:success] = 'login'
+      redirect_to root_path
+    else
+      flash.now[:danger] = 'failed to login'
+      render @new
+    end
+    
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:success] = 'logout'
+    redirect_to root_url
   end
+  
+  private
+  def login(email, password)
+    @user = User.find_by(email: email)
+    
+    if @user && @user.authenticate(password)
+      session[:user_id] = @user.id
+      return true
+    else
+      return false
+    end
+    
+  end
+  
 end
